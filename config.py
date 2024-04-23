9335f3466f3fff013891582d1c8ed8d2f1e41f9d
saurabh2801 <saurabhbhosale335@gmail.com>
2024-04-23 14:43:52 +0530

    model location changed

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os 
-model_path = os.path.join(r"C:\Users\saura\Desktop\Project_aws\Sales_Forecasting\RF_model.pkl")
+current_directory = os.getcwd()
+model_path =os.path.join(current_directory, 'RF_model.pkl')
 json_path = os.path.join(r"C:\Users\saura\Desktop\Project_aws\Sales_Forecasting\project_data.json")
 PORT_NUMBER = 8080
